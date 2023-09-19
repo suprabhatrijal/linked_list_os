fbd55919d443cba3be38f88361e559a367bdabfa
Suprabhat Rijal <goodguysman@gmail.com>
2023-09-19 18:28:20 +0000

    Initial commit

---
+++ list.c
@@ -0,0 +1,45 @@
+// list/list.c
+// 
+// Implementation for linked list.
+//
+// <Author>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "list.h"
+
+list_t *list_alloc() { return NULL; }
+void list_free(list_t *l) {}
+
+void list_print(list_t *l) {}
+
+char * listToString(list_t *l) {
+  char* buf = (char *) malloc(sizeof(char) * 1024);
+  char tbuf[20];
+
+	node_t* curr = l->head;
+  while (curr != NULL) {
+    sprintf(tbuf, "%d->", curr->value);
+    curr = curr->next;
+    strcat(buf, tbuf);
+  }
+  strcat(buf, "NULL");
+  return buf;
+}
+
+int list_length(list_t *l) { return -1; }
+
+void list_add_to_back(list_t *l, elem value) {}
+void list_add_to_front(list_t *l, elem value) {}
+void list_add_at_index(list_t *l, elem value, int index) {}
+
+elem list_remove_from_back(list_t *l) { return -1; }
+elem list_remove_from_front(list_t *l) { return -1; }
+elem list_remove_at_index(list_t *l, int index) { return -1; }
+
+bool list_is_in(list_t *l, elem value) { return false; }
+elem list_get_elem_at(list_t *l, int index) { return -1; }
+int list_get_index_of(list_t *l, elem value) { return -1; }
+
